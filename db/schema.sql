e42f3a1748130e6d42e2225f8905494d05ff286b
Steven Baty <baty_steven@yahoo.com>
2021-04-03 17:37:24 -0600

    add tables departments/roles/employees, add .env file for employee_tracker_db

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,24 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
 CREATE TABLE departments (
-    
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER UNSIGNED,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER UNSIGNED,
+    manager_id INTEGER UNSIGNED
 );
