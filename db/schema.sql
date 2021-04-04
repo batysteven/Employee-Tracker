46b7ec78bec261d42ab96d58e78c02c82ec2d968
Steven Baty <baty_steven@yahoo.com>
2021-04-03 18:30:37 -0600

    created config/connection.js connect to employee_tracker_db, installed dotenv, installed bcrypt, created server.js, installed sequelize

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,29 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS employees;
+-- DROP TABLE IF EXISTS departments;
+-- DROP TABLE IF EXISTS roles;
+-- DROP TABLE IF EXISTS employees;
 
 CREATE DATABASE employee_tracker_db;
 
-CREATE TABLE departments (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-);
+-- CREATE TABLE departments (
+--     id INTEGER PRIMARY KEY,
+--     name VARCHAR(30) NOT NULL
+-- );
 
-CREATE TABLE roles (
-    id INTEGER PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INTEGER UNSIGNED,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
-);
+-- CREATE TABLE roles (
+--     id INTEGER PRIMARY KEY,
+--     title VARCHAR(30) NOT NULL,
+--     salary DECIMAL,
+--     department_id INTEGER UNSIGNED,
+--     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+-- );
 
-CREATE TABLE employees (
-    id INTEGER PRIMARY KEY, 
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER UNSIGNED,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-    manager_id INTEGER UNSIGNED,
-    CONSTRAINT fk_manager FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE SET NULL
-);
+-- CREATE TABLE employees (
+--     id INTEGER PRIMARY KEY, 
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     role_id INTEGER UNSIGNED,
+--     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+--     manager_id INTEGER UNSIGNED,
+--     CONSTRAINT fk_manager FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE SET NULL
+-- );
