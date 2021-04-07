20ce5e9b12e0f446d4613a2eed83339098d4b7f5
Steven Baty <baty_steven@yahoo.com>
2021-04-07 02:12:54 -0600

    updated database.js to include .env for db, updated server.js to include express, app, PORT

--- db/database.js
+++ db/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    database: 'employee_tracker'
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
 });
 
 module.exports = db;
---
+++ server.js
@@ -0,0 +1,15 @@
+const db = require('./db/database');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
