53a8e597af7161ec4b8532f1300cfdef53a4fddc
Steven Baty <baty_steven@yahoo.com>
2021-04-08 00:19:28 -0600

    fixed routes apiRoutes/index.js

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,8 +3,8 @@ const departmentRoutes = require('./departmentRoutes');
 const employeeRoutes = require('./employeeRoutes');
 const roleRoutes = require('./roleRoutes');
 
-router.use('/', departmentRoutes);
-router.use('/', employeeRoutes);
-router.use('/', roleRoutes);
+router.use(departmentRoutes);
+router.use(employeeRoutes);
+router.use(roleRoutes);
 
 module.exports = router;
