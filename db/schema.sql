970eeb43b1b77e558f03e2f2f42034acfe67f25f
Steven Baty <baty_steven@yahoo.com>
2021-04-07 18:15:54 -0600

    updated schema.sql for proper syntax to drop/create/use employee_tracker_db, updated syntax for create tables departments/roles/employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
+
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
@@ -11,16 +15,16 @@ CREATE TABLE roles (
     id INTEGER PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INTEGER UNSIGNED,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
     id INTEGER PRIMARY KEY, 
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER UNSIGNED,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-    manager_id INTEGER UNSIGNED,
-    CONSTRAINT fk_manager FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE SET NULL
+    role_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INTEGER,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
